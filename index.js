3e63ca7ae3d8eab5037abfca56111a5e0eff71c5
mocaffy <mocaffy@mocaffy.com>
2019-03-10 08:58:07 +0900

    commit

--- index.js
+++ index.js
@@ -30,9 +30,7 @@ module.exports.get = form => {
             td.each((i, tdElem) => {
               if (i >= 1 && i <= 4) {
                 // artist title time bpm
-                const text = $(tdElem).text()
-                  .replace(/^\s+/, '')
-                  .replace(/\s+$/, '')
+                const text = $(tdElem).text().replace(/ {2}|\n/g, '')
                 props.push(text !== '' && text !== '？' ? text : null)
               } else if (i === 0 || (i >= 6 && i <= 10)) {
                 // update dl lylic acappella external
