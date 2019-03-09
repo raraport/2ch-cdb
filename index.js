0a51bdd94227a81c25941f8aa734deee6f7c8a68
mocaffy <mocaffy@mocaffy.com>
2019-03-10 08:42:58 +0900

    commit

--- index.js
+++ index.js
@@ -19,68 +19,50 @@ module.exports.get = form => {
       if (!error && response.statusCode === 200) {
         const bodysjis = iconv.decode(body, 'Shift_JIS')
         const $ = cheerio.load(bodysjis)
-        const tbody = $('.search tbody')
-        const tr = tbody.find('tr')
+        const tbodyElem = $('.search tbody')
+        const trElems = tbodyElem.find('tr')
         
-        tr.each((i, trElem) => {
+        trElems.each((lineNum, trElem) => {
           const td = $(trElem).find('td')
-          if (i % 2) {
+          if (lineNum % 2) {
             let props = []
             td.each((i, tdElem) => {
               if (i >= 1 && i <= 4) {
+                // artist title time bpm
                 const text = $(tdElem).text()
                   .replace(/^\s+/, '')
                   .replace(/\s+$/, '')
                 props.push(text !== '' && text !== '？' ? text : null)
               } else if (i === 0 || (i >= 6 && i <= 10)) {
+                // update dl lylic acappella external
                 const link = $(tdElem).find('a')
-                props.push(link.attr('href'))
-                props.push(link.text())
+                props.push(link.attr('href')) // url
+                props.push(link.text())       // type
                 if(i === 7) {
-                  props.push($(tdElem).html().split('<br>')[1])
+                  props.push($(tdElem).html().split('<br>')[1]) // DL count
                 }
               } else if (i === 11) {
-                const span = $(tdElem).find('span')
-                props.push(span.attr('title'))
-                props.push(span.text())
+                // thread
+                const spanElem = $(tdElem).find('span')
+                props.push(spanElem.attr('title'))  // date
+                props.push(spanElem.text())         // thread itle
               }
             })
 
-            result.push({
-              view: props[1] ? {
-                url: props[0],
-                type: props[1]
-              }: null,
-              artist: props[2],
-              title: props[3],
-              time: props[4],
-              bpm: props[5] ? Number(props[5]) : null,
-              update: props[6] ? {
-                url: props[6],
-                type: props[7]
-              }: null,
-              dl: props[8] ? {
-                url: props[8],
-                type: props[9],
-                count: props[10] ? Number(props[10]) : null,
-              }: null,
-              lyric: props[11] ? {
-                url: props[11],
-                type: props[12]
-              }: null,
-              acappella: props[13] ? {
-                url: props[13],
-                type: props[14]
-              }: null,
-              external: props[15] ? {
-                url: props[15],
-                type: props[16]
-              }: null,
-              thread: {
-                date: props[17],
-                title: props[18]
-              }
-            })
+            let item = {}
+            // if (props[1]) item.flag = { url: props[0], type: props[1] }
+            if (props[2]) item.artist = props[2]
+            if (props[3]) item.title = props[3]
+            if (props[4]) item.time = props[4]
+            if (props[5]) item.bpm = Number(props[5])
+            if (props[6]) item.update = { url: props[6], type: props[7] }
+            if (props[8]) item.dl = { url: props[8], type: props[9] }
+            if (props[10]) item.dl.count = Number(props[10])
+            if (props[11]) item.lyric = { url: props[11], type: props[12] }
+            if (props[13]) item.acappella = { url: props[13], type: props[14] }
+            if (props[15]) item.external = { url: props[15], type: props[16] }
+            if (props[17]) item.thread = { date: props[17], title: props[18] }
+            result.push(item)
           }
         })
         resolve(result)
